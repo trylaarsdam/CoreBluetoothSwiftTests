694c64f4e1db865b5f048a5c0b2fcbcf74eae1cd
Todd Rylaarsdam <todd@toddr.org>
2022-02-10 14:19:01 -0600

    updates

--- BLE_Tests/BluetoothManager.swift
+++ BLE_Tests/BluetoothManager.swift
@@ -8,4 +8,126 @@
 import Foundation
 import CoreBluetooth
 
+struct Peripheral: Identifiable, Equatable {
+    let id: Int
+    let name: String
+    let rssi: Int
+    let peripheral: CBPeripheral
+}
 
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    @Published var initialized = false
+    @Published var peripherals = [Peripheral]()
+    @Published var scanning = false
+    @Published var connected = false
+    @Published var discoveredServices = [CBService]()
+
+    var BLEcentral: CBCentralManager!
+    
+    func connect(peripheral: Peripheral) {
+        self.stopScanning()
+        BLEcentral.connect(peripheral.peripheral, options: nil)
+    }
+    
+    func disconnect() {
+        
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            initialized = true
+        }
+        else {
+            initialized = false
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        connected = true
+        peripheral.discoverServices([CBUUID(string: "38FAE295-026C-43F5-AAA9-54064C4251E0")])
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if ((error) != nil) {
+            print("Error discovering services: \(error!.localizedDescription)")
+            return
+        }
+        
+        guard let services = peripheral.services else {
+            return
+        }
+
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+        print("Discovered Services: \(services)")
+        
+        discoveredServices = services
+    }
+    
+    func discoverCharacteristics (service: CBService) {
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print(error)
+        // Handle error
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        connected = false
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        peripheral.delegate = self
+        var peripheralName: String!
+       
+        if let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            peripheralName = name
+        }
+        else {
+            peripheralName = "Unknown device"
+        }
+       
+        let toBeAddedPeripheral = Peripheral(id: peripherals.count, name: peripheralName, rssi: RSSI.intValue, peripheral: peripheral)
+        // print(toBeAddedPeripheral)
+        if(!peripherals.map{ $0.name }.contains(toBeAddedPeripheral.name)) {
+            if(toBeAddedPeripheral.name != "Unknown device") {
+                peripherals.append(toBeAddedPeripheral)
+            }
+        }
+        else {
+            if let toBeUpdatedPeripheralIndex = peripherals.map({$0.name}).firstIndex(of: toBeAddedPeripheral.name) {
+                peripherals[toBeUpdatedPeripheralIndex] = toBeAddedPeripheral
+            }
+        }
+    }
+    
+    func startScanning() {
+            BLEcentral.scanForPeripherals(withServices: [CBUUID(string: "38FAE295-026C-43F5-AAA9-54064C4251E0")], options: nil)
+        self.scanning = true
+    }
+    
+    func stopScanning() {
+        BLEcentral.stopScan()
+        self.scanning = false
+    }
+    
+    func toggleScanning() {
+        if(scanning) {
+            self.stopScanning()
+            self.scanning = false
+        }
+        else {
+            self.startScanning()
+            self.scanning = true
+        }
+    }
+    
+    override init() {
+        super.init()
+
+        BLEcentral = CBCentralManager(delegate: self, queue: nil)
+        BLEcentral.delegate = self
+    }
+}
--- BLE_Tests/ContentView.swift
+++ BLE_Tests/ContentView.swift
@@ -8,9 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var bleManager = BLEManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(bleManager.initialized == true ? "BLE Manager Initialized" : "Waiting for initialization").font(.title2)
+                .padding()
+            HStack {
+                Button(action: {
+                    bleManager.toggleScanning()
+                }) {
+                    Text(bleManager.scanning ? "Stop Scanning" : "Start Scanning")
+                        .fontWeight(.bold)
+                        .foregroundColor(bleManager.scanning ? .red : .green)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(bleManager.scanning ? .red : .green, lineWidth: 5)
+                        )
+                }
+                if(bleManager.connected) {
+                    Button(action: {
+                        bleManager.disconnect()
+                    }) {
+                        Text("Disconnect")
+                            .fontWeight(.bold)
+                            .foregroundColor(bleManager.connected ? .red : .green)
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(bleManager.connected ? .red : .green, lineWidth: 5)
+                            )
+                    }
+                }
+            }.padding(.bottom)
+            if(bleManager.connected) {
+                Text("Services List").font(.title2)
+                List {
+                    ForEach(bleManager.discoveredServices, id: \.self) { service in
+                        Text(service.uuid.uuidString)
+                    }
+                }
+            }
+            else {
+                List {
+                    ForEach(bleManager.peripherals) { peripheral in
+                        HStack {
+                            Text(peripheral.name).bold()
+                            Spacer()
+                            Button(String(peripheral.rssi)){
+                                bleManager.connect(peripheral: peripheral)
+                            }.foregroundColor(.blue)
+                        }.padding()
+                    }
+                }
+            }
+        }
     }
 }
 
