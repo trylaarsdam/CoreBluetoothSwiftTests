f1c84c6c54d7da29ff78342ec840fe234077e5e3
Todd Rylaarsdam <todd@toddr.org>
2022-02-10 11:05:41 -0600

    init

---
+++ BLE_Tests/BLE_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_TestsApp.swift
+//  BLE_Tests
+//
+//  Created by Todd Rylaarsdam on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BLE_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLE_Tests/BluetoothManager.swift
@@ -0,0 +1,11 @@
+//
+//  BluetoothManager.swift
+//  BLE_Tests
+//
+//  Created by Todd Rylaarsdam on 2/10/22.
+//
+
+import Foundation
+import CoreBluetooth
+
+
